3a27849b825b54270ae97778c53208c145d4c4c5
Nikita Krasheninnik <krasheninnik99@gmail.com>
2021-04-13 21:14:52 +0700

    add simple command

---
+++ Inverse-solver/ViewModel/Commands/SimpleCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Inverse_solver.ViewModel.Commands
+{
+    public class SimpleCommand : ICommand
+    {
+        public TaskViewModel ViewModel { get; set; }
+        public SimpleCommand(TaskViewModel viewModel)
+        {
+            ViewModel = viewModel;
+        }
+     
+        
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            // execute this method in any cases
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            this.ViewModel.SimpleMethod();
+        }
+    }
+}
--- Inverse-solver/ViewModel/TaskViewModel.cs
+++ Inverse-solver/ViewModel/TaskViewModel.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Inverse_solver.Model;
+using Inverse_solver.ViewModel.Commands;
 
 namespace Inverse_solver.ViewModel
 {
     public class TaskViewModel : INotifyPropertyChanged
     {
+        public SimpleCommand SimpleCommand { get; set; }
+        public TaskViewModel()
+        {
+            this.SimpleCommand = new SimpleCommand(this);
+        }
+         
+        public void SimpleMethod()
+        {
+            Debug.WriteLine("Hello");
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         // Caller Member Name allows not to pass property name in the function,
