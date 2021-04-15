c3cb62c8bffc6e2dc903322fd5153d2207ccbf41
Nikita Krasheninnik <krasheninnik99@gmail.com>
2021-04-15 15:31:17 +0700

    implement template heapmap

--- Inverse-solver/ViewModel/TaskViewModel.cs
+++ Inverse-solver/ViewModel/TaskViewModel.cs
@@ -11,6 +11,9 @@ using Inverse_solver.Model;
 using Inverse_solver.ViewModel.Commands;
 using Inverse_solver.ViewModel.Converters;
 using Inverse_solver.Views;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 
 namespace Inverse_solver.ViewModel
 {
@@ -21,7 +24,58 @@ namespace Inverse_solver.ViewModel
             this.task = new CalculatingTask();
             this.OpenSettingsFormCommand = new OpenSettingsFormCommand(this);
             this.InitTaskCommand = new InitTaskCommand(this);
+
+            var model = new PlotModel { Title = "Results" };
+            // Weekday axis (horizontal)
+
+            // CATEGORY AXIS CODE====================================
+            var categoryaxis = new CategoryAxis();
+            for (int i = 0; i < 7; i++)
+            {
+                int value = i * 100;
+                categoryaxis.ActualLabels.Add(value.ToString());
+
+            }
+            categoryaxis.Angle = 0; // 40;
+            model.Axes.Add(categoryaxis);
+
+            // Cake type axis (vertical)
+            model.Axes.Add(new LinearAxis());
+            // Color axis
+            model.Axes.Add(new LinearColorAxis
+            {
+                Palette = OxyPalettes.Hot(200)
+            });
+
+            var rand = new Random();
+            var data = new double[7, 5];
+            for (int x = 0; x < 5; ++x)
+            {
+                for (int y = 0; y < 7; ++y)
+                {
+                    data[y, x] = rand.Next(0, 200) * (0.13 * (y + 1));
+                }
+            }
+
+            var heatMapSeries = new HeatMapSeries
+            {
+                X0 = 0,
+                X1 = 6,
+                Y0 = 0,
+                Y1 = 4,
+                // XAxisKey = "WeekdayAxis",
+                // YAxisKey = "CakeAxis",
+                RenderMethod = HeatMapRenderMethod.Rectangles,
+                LabelFontSize = 0.2, // neccessary to display the label
+                Data = data,
+            };
+
+            model.Series.Add(heatMapSeries);
+            this.MyModel = model;
         }
+
+        public PlotModel MyModel { get; set; }
+
 
         CalculatingTask task { get; set; }
 
@@ -38,7 +92,7 @@ namespace Inverse_solver.ViewModel
         }
 
         public void InitTask()
-        { 
+        {
             task.InitInverseTask(Hx, Hy, X0, Y0, Z0,
                 MeasuredValues.ToArray(), MeasuredValues.Count,
                 Xstart, Xend, XstepsAmount,
@@ -63,23 +117,6 @@ namespace Inverse_solver.ViewModel
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
         }
 
-        // Proooooops: 
-        // Test developming props, need delete latter
-        private int _number1;
-        public int Number1
-        {
-            get { return _number1; }
-            set { _number1 = value; OnPropertyChanged("Number3"); }
-        }
-
-        private int _number2;
-        public int Number2
-        {
-            get { return _number2; }
-            set { _number2 = value; OnPropertyChanged("Number3"); }
-        }
-
-        public int Number3 { get => CalculatingTask.GetSumOf(Number1, Number2); }
 
         public int ElemsSize { get; set; }
 
--- Inverse-solver/Views/MainWindow.xaml.cs
+++ Inverse-solver/Views/MainWindow.xaml.cs
@@ -1,19 +1,4 @@
-﻿using Inverse_solver.ViewModel;
-using Inverse_solver.Views;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Inverse_solver
 {
@@ -26,5 +11,6 @@ namespace Inverse_solver
         {
             InitializeComponent();
         }
+
     }
 }
